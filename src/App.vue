e0e0819dc5274b2173e650f91bbc7b57bbca310f
Rova Fitia <rovafiti@gmail.com>
2024-08-12 23:56:22 +0300

    Chore: init project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<!-- Gére les templates HTML (modèle) -->
+<template>
+    <h1>Bonjour {{ firstName.toUpperCase() }}</h1>
+    <p>Comment ça va ?</p>
+</template>
+
+<!-- Gère les Javascript -->
+<script setup>
+    const firstName = "John Doe"
+</script>
+
+<!-- Gère les css -->
+<style>
+    h1 {
+        color: red;
+    }
+</style>
