a4bc0aabf29beb01d4a8bda3d2b46f5ad8ee8668
Rova Fitia <rovafiti@gmail.com>
2024-08-21 13:14:28 +0300

    Add new concept of Vue js

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,71 @@
 <!-- Gére les templates HTML (modèle) -->
 <template>
-    <h1>Bonjour {{ firstName.toUpperCase() }}</h1>
-    <p>Comment ça va ?</p>
+    <p :id="`count-${count}`" :style="{color : count >= 5 ? 'red' : 'green'}" :class="{active: count > 5 }" >Compteur : {{ count }}</p>
+    <div v-if="count >= 5">Vous avez cliquer plus de 5 fois</div>
+    <div v-else>Vous avez cliquer moins de 5 fois</div>
+    <button @click="increment">Incrément</button>
+    <button @click="decrement">Décrément</button>
+
+    <!-- Boucle avec Vue -->
+    <hr/>
+    <button @click="sortMovies">Réorganiser</button>
+
+    <form action="" @submit.prevent="addMovie">
+        <input type="text" placeholder="Enter your movie" v-model="movieName">
+        <button>Add</button>
+    </form>
+    <ul>
+        <li v-for="movie in movies" :key="movie">
+            {{ movie }} <button @click="deleteMovie(movie)">Sup</button>
+        </li>
+    </ul>
+
 </template>
 
 <!-- Gère les Javascript -->
 <script setup>
-    const firstName = "John Doe"
+import { ref } from 'vue';
+
+const count = ref(0)
+
+const increment = ()=> {
+    count.value ++
+}
+
+const decrement = ()=> {
+    count.value --
+}
+
+// Tableau :
+const movies = ref([
+    'Matrix',
+    'Lilo & Stich',
+    'Avengers'
+])
+
+const deleteMovie = (movie) => {
+    movies.value = movies.value.filter(m => m !== movie)
+}
+
+const sortMovies = () => {
+    // movies.value = movies.value.sort()
+    movies.value.sort((a,b) => a > b ? 1 : -1)
+}
+
+// Submit :
+const movieName = ref('')
+
+const addMovie = () => {
+    movies.value.push(movieName.value)
+    // vider le champ :
+    movieName.value = ""
+}
+
 </script>
 
 <!-- Gère les css -->
 <style>
-    h1 {
-        color: red;
+    .active {
+        font-size: 1.5rem;
     }
 </style>
