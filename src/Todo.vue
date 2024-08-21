e311c44a6b91155ac6547a38f66d82f18a300853
Rova Fitia <rovafiti@gmail.com>
2024-08-21 20:52:42 +0300

    Feat: add Todo List

---
+++ src/Todo.vue
@@ -0,0 +1,68 @@
+<template>
+    <form action="" @submit.prevent="addTodos">
+        <fieldset role="group">
+            <input type="text" placeholder="Entrer votre tâche" v-model="newTodo">
+            <button :disabled="newTodo.length === 0">Ajouter</button>
+        </fieldset>
+    </form>
+    <div v-if="todos.length === 0">Vous n'avez pas fait aucune tâche :(</div>
+    <div v-else>
+        <ul>
+            <li
+                v-for="todo in sortTodo()"
+                :key="todo.date"
+                :class="{completed : todo.completed}"
+            >
+                <label>
+                    <input type="checkbox" v-model="todo.completed">
+                    {{ todo.title }}
+                </label>
+            </li>
+        </ul>
+
+        <label>
+            <input type="checkbox" v-model="hideTodo">
+            Masquer les tâches complétées
+        </label>
+    </div>
+
+</template>
+
+<script setup>
+    import {ref} from 'vue'
+    const todos = ref([
+        { "title": "Acheter la propriété 'Rue de la Paix'", "completed": false, "date": 20240730 },
+        { "title": "Construire un hôtel sur 'Avenue Foch'", "completed": true, "date": 20240730 },
+        { "title": "Éviter la case prison", "completed": false, "date": 20240730 }
+    ])
+    const newTodo = ref('')
+    const hideTodo = ref(false)
+
+    const addTodos = () => {
+        todos.value.push({
+            title: newTodo.value ,
+            completed: false,
+            date: Date.now()
+        })
+
+        newTodo.value = ""
+    }
+
+    // Order of todo :
+    const sortTodo = () => {
+        const sortTodo = todos.value.toSorted((a, b) => a.completed > b.completed ? 1 : -1)
+
+        if(hideTodo.value === true) {
+            return sortTodo.filter(t => t.completed === false)
+        }
+
+        return sortTodo
+    }
+</script>
+
+<style>
+    .completed {
+        opacity: .5;
+        text-decoration: line-through;
+    }
+</style>
